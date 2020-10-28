ffef01db1da68e2b1b8339086b82d11a524e5264
Tanvi Aditya <tanviaditya1@gmail.com>
2020-10-28 17:10:37 +0530

    API linked

--- src/ActionProvider.js
+++ src/ActionProvider.js
@@ -1,8 +1,35 @@
+import axios from 'axios';
 class ActionProvider {
     constructor(createChatBotMessage, setStateFunc, createClientMessage) {
       this.createChatBotMessage = createChatBotMessage;
       this.setState = setStateFunc;
       this.createClientMessage = createClientMessage;
+    }
+    greet(str) {
+      const greetingMessage = this.createChatBotMessage(str);
+      this.updateChatbotState(greetingMessage)
+    }
+    function1=()=>{
+      axios.get("http://127.0.0.1:8000/predict/",{
+        params:{
+          "data":"Hi"
+        }
+      })
+      .then(function (response){
+        this.setState({reply:response.data.data})
+        console.log(this.state)
+        // const greetingMessage = this.createChatBotMessage(response.data.data);
+        // this.updateChatbotState(greetingMessage)
+      })
+      .catch(function(err){
+        console.log(err)
+      });
+    }
+
+    updateChatbotState(message) {
+      this.setState(prevState => ({
+        ...prevState, messages: [...prevState.messages, message]
+      }))
     }
   }
   
--- src/config.js
+++ src/config.js
@@ -1,7 +1,15 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)]
+  initialMessages: [createChatBotMessage(`Hey!! How can I help you?`)],
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: "#276B1E",
+    },
+    chatButton: {
+      backgroundColor: "#276B1A",
+    },
+  }
 }
 
 export default config
